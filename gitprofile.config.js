ff4a0ae6e3891463fb3cee76a7afae758f39e30d
WildCat Studio <djacidburn1877@gmail.com>
2023-12-20 19:04:42 -0500

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -3,8 +3,8 @@
 const config = {
   github: {
     username: 'djacidfx', // Your GitHub org/user name. (Required)
-    sortBy: 'stars', // stars | updated
-    limit: 10, // How many projects to display.
+    sortBy: 'updated', // stars | updated
+    limit: 20, // How many projects to display.
     exclude: {
       forks: false, // Forked projects will not be displayed if set to true.
       projects: [], // These projects will not be displayed. example: ['my-project1', 'my-project2']
